5f193a1a02162feb0ef1791686a9af49abd5f8ff
yu1011 <42330076+yu1011@users.noreply.github.com>
2020-05-13 15:55:32 +0800

    Update 胡立.js

--- 胡立.js
+++ 胡立.js
@@ -19,7 +19,21 @@ for (i from 1 to n) do
   else if (i % 3 ===0) print "fizz"
   else if (i % 5 ===0) print "buzz"
 end for
-
+=========================================
+  function fizzy(){
+    for(i =0; i <= 100; i++){
+        if( i % 15 == 0){
+            console.log("fizzbuzz")
+        }else if(i % 5 == 0){
+            console.log("fizz")  
+    } else if(i % 3 ==0){
+        console.log("buzz")
+}else{
+    console.log("i"); 
+}
+}
+}
+fizzy();
 =========================================
 //Unit1.6：實戰練習：找最小值 
 //從陣列找最小值
